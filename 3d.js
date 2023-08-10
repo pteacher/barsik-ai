fd9f07664bd35799cca70f245fd208b44be501eb
pteacher <subfear@mail.ru>
2023-08-10 10:46:54 +0600

    add greeting animation

--- 3d.js
+++ 3d.js
@@ -224,8 +224,40 @@ camera.position.z = 5;
 // const controls = new OrbitControls(camera, renderer.domElement);
 // controls.update();
 
-const ambient = new THREE.AmbientLight(0x222222, 8.9);
-scene.add(ambient);
+const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 2 );
+hemiLight.color.setHSL( 0.6, 1, 0.6 );
+hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+hemiLight.position.set( 0, 50, 0 );
+scene.add( hemiLight );
+
+const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+scene.add( hemiLightHelper );
+
+//
+
+const dirLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
+dirLight.color.setHSL( 0.1, 1, 0.95 );
+dirLight.position.set( 2, 3, 50 );
+dirLight.position.multiplyScalar( 0.5 );
+scene.add( dirLight );
+
+dirLight.castShadow = true;
+
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+
+const d = 50;
+
+dirLight.shadow.camera.left = - d;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = - d;
+
+dirLight.shadow.camera.far = 3500;
+dirLight.shadow.bias = - 0.0001;
+
+const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
+scene.add( dirLightHelper );
 
 const loader = new GLTFLoader();
 loader.load('./public/model.glb', function (gltf) {
@@ -243,6 +275,12 @@ loader.load('./public/model.glb', function (gltf) {
 
     // actions["Speaking"].setEffectiveWeight(1);
     // prepareCrossFade(actions["Greeting"], actions["Speaking"]);
+    setTimeout(() => {
+        prepareCrossFade(actions["Idle"], actions["greeting"])
+        setTimeout(() => {
+            prepareCrossFade(actions["greeting"], actions["Idle"])
+        }, 4000)
+    }, 5000)
 
     setInterval(() => {
         actions["Blinking"].weight = 1;
